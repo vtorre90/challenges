99e9dda81661e8beaf82b6d87be3cb8175bec139
vtorre90 <87823112+vtorre90@users.noreply.github.com>
2021-09-26 21:33:41 +0200

    Add files via upload

---
+++ Soft/stack/solutions/stack.py
@@ -0,0 +1,28 @@
+# push(item) - Add an item to the top of the stack.
+# pop() - Remove the top item from the stack.
+# peek() - Returns a copy of the top item in the stack.
+# isEmpty() - Return a boolean indicating whether or not the stack is empty.
+# size() - Return the number of items in the stack.
+
+fruit_list = ['banana', 'apple', 'plumb', 'grapes', 'peach']
+
+#Add an item to the top of the stack
+def push(fruit_list):
+    var='watermelon'
+    fruit_list.insert(0,var)
+    print (fruit_list)
+
+push(fruit_list)
+
+
+#pop() - Remove the top item from the stack.
+
+fruit_list = ['banana', 'apple', 'plumb', 'grapes', 'peach']
+def removeTop(fruit_list):
+    fruit_list.pop(0)
+    print (fruit_list)
+
+removeTop(fruit_list)
+
+
+
